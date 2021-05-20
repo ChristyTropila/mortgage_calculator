5f5ff9045ac5151e4ae7ec62c7c62f6f4870ed45
ChristyTropila <tropila04@gmail.com>
2021-05-20 14:36:32 -0400

    Theme colors

---
+++ src/Components/HeaderComponent/Header.js
@@ -0,0 +1,8 @@
+
+
+export const Header = () => {
+
+    return(
+        <h1>hello from header</h1>
+    )
+}
---
+++ src/Components/HomeComponent/HomeLanding.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Header} from '../HeaderComponent/Header'
+import styles from './HomeLandingCss.css'
+
+export const HomeLanding = () => {
+
+    return(
+        <div className='container'>
+            <div className='top-container'>
+                <Header/>
+            </div>
+        </div>
+    )
+};
